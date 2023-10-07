eb0566b0f7a3689c255bfa6c6d8747cee73e840c
JoseMiguel01012570345 <jmperezperez25@gmail.com>
2023-10-07 18:02:19 -0400

    fixing several fitures

--- docs/portafolio_script.js
+++ docs/portafolio_script.js
@@ -1,181 +1,86 @@
-var linkedin = document.querySelector(".linkedin");
-var wsp = document.querySelector(".wsp");
-var email = document.querySelector(".email");
-var telegram = document.querySelector(".telegram");
-var github = document.querySelector(".github");
-
-var linkedinImg = document.querySelector(".linkedin-img");
-var wspImg = document.querySelector(".wsp-img");
-var emailImg = document.querySelector(".email-img");
-var telegramImg = document.querySelector(".telegram-img");
-var githubImg = document.querySelector(".github-img");
-var myInfo = document.querySelector(".my-info");
+let wsp = document.querySelector(".wsp");
+let email = document.querySelector(".email");
+let telegram = document.querySelector(".telegram");
+let linkedinImg = document.querySelector(".linkedin-img");
+let wspImg = document.querySelector(".wsp-img");
+let emailImg = document.querySelector(".email-img");
+let telegramImg = document.querySelector(".telegram-img");
+let githubImg = document.querySelector(".github-img");
+let myInfo = document.querySelector(".my-info");
+let myTable = document.querySelector(".myTable");
 
 //---------------------------------------------------------------------------------
 linkedinImg.addEventListener("mouseenter", (event) => {
-    linkedin.style.setProperty("display", "block");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+});
 wspImg.addEventListener("mouseenter", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "block");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur( 1px)");
+});
 emailImg.addEventListener("mouseenter", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "block");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+});
 telegramImg.addEventListener("mouseenter", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "block");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+});
 githubImg.addEventListener("mouseenter", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "block");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+});
 
 //---------------------------------------------------------------------------------
 linkedinImg.addEventListener("mouseout", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
+  myTable.style.setProperty("filter", "none");
+});
 wspImg.addEventListener("mouseout", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
+  myTable.style.setProperty("filter", "none");
+});
 emailImg.addEventListener("mouseout", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
+  myTable.style.setProperty("filter", "none");
+});
 telegramImg.addEventListener("mouseout", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
+  myTable.style.setProperty("filter", "none");
+});
 githubImg.addEventListener("mouseout", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
+  myTable.style.setProperty("filter", "none");
+});
 
 //---------------------------------------------------------------------------------
 linkedinImg.addEventListener("click", (event) => {
-    linkedin.style.setProperty("display", "block");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+});
 wspImg.addEventListener("click", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "block");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+  wsp.style.setProperty("display", "block");
+  email.style.setProperty("display", "none");
+  telegram.style.setProperty("display", "none");
+  wsp.scrollIntoView({ behavior: "smooth", block: "start" });
+});
 emailImg.addEventListener("click", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "block");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+  wsp.style.setProperty("display", "none");
+  email.style.setProperty("display", "block");
+  telegram.style.setProperty("display", "none");
+  email.scrollIntoView({ behavior: "smooth", block: "start" });
+});
 telegramImg.addEventListener("click", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "block");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+  wsp.style.setProperty("display", "none");
+  email.style.setProperty("display", "none");
+  telegram.style.setProperty("display", "block");
+  telegram.scrollIntoView({ behavior: "smooth", block: "start" });
+});
 githubImg.addEventListener("click", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "block");
-    myInfo.style.setProperty("filter", "blur(10px)")
-})
+  myTable.style.setProperty("filter", "blur(2px)");
+});
 //---------------------------------------------------------------------------------
-linkedinImg.addEventListener("mouseup", (event) => {
-    linkedin.style.setProperty("display", "block");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
-wspImg.addEventListener("mouseup", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "block");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
-emailImg.addEventListener("mouseup", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "block");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
-telegramImg.addEventListener("mouseup", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "block");
-    github.style.setProperty("display", "none");
-    myInfo.style.setProperty("filter", "none")
-})
-githubImg.addEventListener("mouseup", (event) => {
-    linkedin.style.setProperty("display", "none");
-    wsp.style.setProperty("display", "none");
-    email.style.setProperty("display", "none");
-    telegram.style.setProperty("display", "none");
-    github.style.setProperty("display", "block");
-    myInfo.style.setProperty("filter", "none")
-})
-//--------------------------------------------------------------------------------------
+
+document.addEventListener("touchstart", (event) => {
+  if (
+    event.target != wspImg &&
+    event.target != telegramImg &&
+    event.target != emailImg
+  ) {
+    myTable.style.setProperty("filter", "none");
+  }
+});
+
+//---------------------------------------------------------------------------------
